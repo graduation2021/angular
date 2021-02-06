1564a09a447c78d9484f83c4ace6d884338ff67f
Tomek <tmajcher21@gmail.com>
2021-02-06 19:05:51 +0100

    asd

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 app.use(express.static('./dist/front-graduation'));
 
-app.get('/*', (req, res) =>
+app.get('/', (req, res) =>
      res.sendFile('index.html', {root: __dirname}));
 
 app.listen(process.env.PORT || 8080);
